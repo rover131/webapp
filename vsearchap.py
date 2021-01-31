7899ac05634244afed5b216bd7bf0133e16cc5c2
rover131 <rover102a@mail.ru>
2021-02-01 02:20:28 +0300

    mark 8 (добавлена многопоточность и полуисключение)

--- vsearchap.py
+++ vsearchap.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
+from threading import Thread
 #from time import sleep
 import sys
 sys.path.append(r"C:\python\appweb\module")
@@ -12,20 +13,15 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                           'password': 'vsearchpasswd',
                           'database': 'vsearchlogDB', }
 
-def log_request(req: 'flask_request',res: str) -> None:
-    #sleep(15)
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                      (phrase, letters, ip, browser_string, results)
-                      values
-                      (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL,(req.form['phrase'],
-                             req.form['letters'],
-                             req.remote_addr,
-                             req.user_agent.browser,
-                             res))
-    with open ('vsearch.log','a') as log:
-        print(req.form,req.remote_addr,req.user_agent,res, file=log, sep='|')
+@app.route('/login')
+def do_login():
+    session['logged_in'] = True
+    return 'You are logged in'
+
+@app.route('/logout')
+def do_logout():
+    session.pop('logged_in')
+    return 'You are logged out'
 
 @app.route('/')
 @app.route('/entry')
@@ -34,12 +30,34 @@ def entry_page() -> str:
 
 @app.route('/search', methods=['POST'])
 def do_search() -> 'html':
+
+    try:
+        @copy_current_request_context
+        def log_request(req: 'flask_request',res: str) -> None:
+            #sleep(15)
+            with UseDatabase(app.config['dbconfig']) as cursor:
+                _SQL = """insert into log
+                              (phrase, letters, ip, browser_string, results)
+                              values
+                              (%s, %s, %s, %s, %s)"""
+                cursor.execute(_SQL,(req.form['phrase'],
+                                     req.form['letters'],
+                                     req.remote_addr,
+                                     req.user_agent.browser,
+                                     res))
+            with open ('vsearch.log','a') as log:
+                print(req.form,req.remote_addr,req.user_agent,res, file=log, sep='|')
+    except Exception as err:
+        print('Ошибка в записи результата в БД', str(err))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Результаты:'
     results = str(searchlet(phrase, letters))
+
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('Ошибка подключения БД ',str(err))
     return render_template('results.html',
@@ -71,15 +89,6 @@ def view_the_log() -> 'html':
         print('Что-то случилось ', str(err))
     return 'Error'
 
-@app.route('/login')
-def do_login():
-    session['logged_in'] = True
-    return 'You are logged in'
-
-@app.route('/logout')
-def do_logout():
-    session.pop('logged_in')
-    return 'You are logged out'
 
 app.secret_key = 'password'
 if __name__ == '__main__':
